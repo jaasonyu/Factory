97ad01d9837b625d2d75f7cc7dfd7af02b5879bb
Jason <jason_yu25@milton.edu>
2022-12-02 13:01:30 -0500

    Added a note on parameters

--- src/RunMyProgram.java
+++ src/RunMyProgram.java
@@ -16,6 +16,7 @@ public class RunMyProgram {
         greenlantern.width = 32;
         greenlantern.print();
         avatar starfire = new avatar(330,200,15,28,178,300,"Starbolt blast","hero","too many powers");
+        //this parameter allows you to simplify what we did for green lantern
         starfire.print();
         avatar namor = new avatar (440,220,60,180,175,30,"super strength","villain","the sun");
         namor.print();
